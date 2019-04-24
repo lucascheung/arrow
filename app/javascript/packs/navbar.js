f4eac79bf8452f2dc37e1ebba88ae13769335897
Lucas Cheung <lucascheungls@gmail.com>
2019-04-24 18:41:31 +0100

    front banner animation

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -3,7 +3,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("logo-image").style.width = "50px";
-    document.querySelector('.navbar-lewagon').style.backgroundColor = '#343c4c';
+    document.querySelector('.navbar-lewagon').style.backgroundColor = '#112c3e';
   } else {
     document.getElementById("logo-image").style.width = "100px";
     document.querySelector('.navbar-lewagon').style.backgroundColor = 'transparent'
