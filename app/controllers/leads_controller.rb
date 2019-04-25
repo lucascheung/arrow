ed4bdea0136797fd92c9d083fe7749a45293588a
Lucas Cheung <lucascheungls@gmail.com>
2019-04-25 17:43:35 +0100

    add line break

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -6,7 +6,7 @@ class LeadsController < ApplicationController
     @lead = Lead.new(lead_params)
     @lead.save
     card_title = "#{@lead.first_name} #{@lead.last_name}"
-    card_description = "First name: #{@lead.first_name}            Last name: #{@lead.last_name}                 Email: #{@lead.email}               Company: #{@lead.company}                Phone number: #{@lead.phone_number}                    Message: #{@lead.message}"
+    card_description = "Name: #{@lead.first_name} #{@lead.last_name}%0D%0AEmail: #{@lead.email}%0D%0ACompany: #{@lead.company}%0D%0APhone number: #{@lead.phone_number}%0D%0AMessage: #{@lead.message}"
     url = URI("https://api.trello.com/1/cards?name=#{card_title}&desc=#{card_description}&pos=bottom&idList=5cc1bf584bd2272619745152&keepFromSource=all&key=#{ENV['TRELLO_API_KEY']}&token=#{ENV['TRELLO_API_TOKEN']}")
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
