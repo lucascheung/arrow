d095b80eed74572c1bb6f84a9a7ca6544cdf0d9b
Lucas Cheung <lucascheungls@gmail.com>
2019-04-25 17:32:23 +0100

    add trello

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -5,16 +5,16 @@ class LeadsController < ApplicationController
   def create
     @lead = Lead.new(lead_params)
     @lead.save
-    # card_title = "#{@lead.first_name} #{@lead.last_name}"
-    # card_description = "First name: #{@lead.first_name}            Last name: #{@lead.last_name}                 Email: #{@lead.email}               Company: #{@lead.company}                Phone number: #{@lead.phone_number}                    Message: #{@lead.message}"
-    # url = URI("https://api.trello.com/1/cards?name=#{card_title}&desc=#{card_description}&pos=bottom&idList=5cc1bf584bd2272619745152&keepFromSource=all&key=#{ENV['TRELLO_API_KEY']}&token=#{ENV['TRELLO_API_TOKEN']}")
-    # http = Net::HTTP.new(url.host, url.port)
-    # http.use_ssl = true
-    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    card_title = "#{@lead.first_name} #{@lead.last_name}"
+    card_description = "First name: #{@lead.first_name}            Last name: #{@lead.last_name}                 Email: #{@lead.email}               Company: #{@lead.company}                Phone number: #{@lead.phone_number}                    Message: #{@lead.message}"
+    url = URI("https://api.trello.com/1/cards?name=#{card_title}&desc=#{card_description}&pos=bottom&idList=5cc1bf584bd2272619745152&keepFromSource=all&key=#{ENV['TRELLO_API_KEY']}&token=#{ENV['TRELLO_API_TOKEN']}")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    # request = Net::HTTP::Post.new(url)
+    request = Net::HTTP::Post.new(url)
 
-    # response = http.request(request)
+    response = http.request(request)
   end
 
   private
